d3613ebe8b4900a263371120b1218574655f16f2
danila <developer.permogorsky@gmail.com>
2025-11-23 14:38:50 +0000

    Create kernel with hello-world output using VGA

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,18 @@
 #![no_main] // disable all Rust-level entry points
 
 use core::panic::PanicInfo;
-
-#[unsafe(no_mangle)] // don't mangle the name of this function
-pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named '_start' by default
-    loop {
-
-    }
-}
+mod vga_buffer;
 
 /// This function is called on panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop{}
+}
+#[unsafe(no_mangle)] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+
+    loop {
+
+    }
 }
---
+++ src/vga_buffer.rs
@@ -0,0 +1,97 @@
+#[allow(dead_code)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[repr(u8)]
+pub enum Colour {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+struct ColourCode(u8);
+
+impl ColourCode {
+    fn new(foreground: Colour, background: Colour) -> ColourCode {
+        ColourCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct ScreenChar {
+    ascii_character: u8,
+    colour_code: ColourCode,
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+#[repr(transparent)]
+struct Buffer {
+    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+pub struct Writer {
+    column_position: usize,
+    colour_code: ColourCode,
+    buffer: &'static mut Buffer,
+}
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
+
+                let colour_code = self.colour_code;
+                self.buffer.chars[row][col] = ScreenChar {
+                    ascii_character: byte,
+                    colour_code,
+                };
+                self.column_position += 1;
+            }
+        }
+    }
+    fn new_line(&mut self) {}
+
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                // printable ASCII byte or newline
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                // not part of printable ASCII range
+                _ => self.write_byte(0xfe),
+            }
+        }
+    }
+}
+pub fn print_something() {
+    let mut writer = Writer {
+        column_position: 0,
+        colour_code: ColourCode::new(Colour::Yellow, Colour::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
+
+    writer.write_byte(b'H');
+    writer.write_string("ello ");
+    writer.write_string("Wörld!");
+}
